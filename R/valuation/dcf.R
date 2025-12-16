5568dea19851b1d1a0dfcebde000eab708611de8
modarex <102308988+modarex@users.noreply.github.com>
2025-12-16 00:40:51 -0500

    Update dcf.R

--- R/valuation/dcf.R
+++ R/valuation/dcf.R
@@ -1,11 +1,9 @@
-# R/valuation/dcf.R
 suppressPackageStartupMessages({
   library(dplyr)
   library(tibble)
 })
 
 project_fcf <- function(last_fcf, growth, years = 5) {
-  # growth as decimal (e.g., 0.08)
   tibble(
     year = 1:years,
     fcf = last_fcf * (1 + growth)^(1:years)
@@ -13,12 +11,10 @@ project_fcf <- function(last_fcf, growth, years = 5) {
 }
 
 dcf_value <- function(fcf_proj, wacc, terminal_g, net_debt = 0, shares_out = 1) {
-  # wacc, terminal_g in decimals
   years <- nrow(fcf_proj)
   disc <- (1 + wacc)^(fcf_proj$year)
   pv_fcf <- sum(fcf_proj$fcf / disc, na.rm = TRUE)
 
-  # Gordon terminal value at end of projection horizon
   fcf_n <- fcf_proj$fcf[years]
   tv <- (fcf_n * (1 + terminal_g)) / (wacc - terminal_g)
   pv_tv <- tv / (1 + wacc)^years
